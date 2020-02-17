c752b640d87bfd068d0fb63542ae0dbb79a3922a
Sterba Patrik <sterba@cngroup.dk>
2020-02-17 20:12:49 +0100

    Preparation for deployment

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
+const https = require('https');
 
 const routes = require('./routes');
+
+const MONGDB_URI = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@nodeapp-cuhlh.mongodb.net/${process.env.MONGODB_DEFAULT_DB}?retryWrites=true&w=majority`;
 
 const app = express();
 
 // Body parser
 app.use(bodyParser.json());
+
+// SSL/TSL
+const privateKey = fs.readFileSync('server.key');
+const certificate = fs.readFileSync('server.cert');
 
 // Cors
 app.use((req, res, next) => {
@@ -29,14 +41,29 @@ app.use((error, req, res, next) => {
   req.status(status).json({ message, data });
 });
 
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, 'access.log'),
+  { flags: 'a' },
+);
+
 // Routes
 app.use('/', routes);
 
+// Security
+app.use(helmet());
+
+// Compression of assets
+app.use(compression());
+
+// Request logging
+app.use(morgan('combined', { stream: accessLogStream }));
+
 // Connection
 mongoose
-  .connect(
-    'mongodb+srv://root:1026Orlova@nodeapp-cuhlh.mongodb.net/stuff-sharer?retryWrites=true&w=majority',
-    { useNewUrlParser: true, useUnifiedTopology: true },
+  .connect(MONGDB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    // https
+    //   .createServer({ key: privateKey, cert: certificate }, app)
+    app.listen(process.env.PORT || 5555),
   )
-  .then(() => app.listen(4000))
   .catch(err => console.log(err));
